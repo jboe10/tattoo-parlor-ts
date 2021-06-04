88af60058a3ba72ac7e3984368ad5abd8f49ab88
jboe10 <jboesurf@gmail.com>
2021-06-04 02:21:14 -0700

    artist item started

--- src/components/artists/ArtistItem.tsx
+++ src/components/artists/ArtistItem.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
 
 interface ArtistItemProps {
-
+  orientedLeft: boolean;
+  name: string;
+  email: string;
+  ig: string;
 }
 
-export const ArtistItem: React.FC<ArtistItemProps> = () => {
+
+export const ArtistItem: React.FC<ArtistItemProps> = (props) => {
   return (
     <>
+      { props.orientedLeft && 
+        <div className="artist-item">
+          <img/>
+          <div className="artist-info">
+
+          </div>
+        </div>
+      }
+      {
+        !props.orientedLeft && 
+        <div className="artist-item">
+          <div className="artist-info">
+            
+          </div>
+          <img/>
+        </div>
+      }
     </>
   );
 }
--- src/components/artists/Artists.tsx
+++ src/components/artists/Artists.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { ArtistItem } from './ArtistItem'
 
-interface ArtistsProps {
-
-}
-
-export const Artists: React.FC<ArtistsProps> = () => {
+export const Artists: React.FC<null> = () => {
   return (
-    <>
-    </>
+    <section className="artist-section">
+      <ArtistItem
+        orientedLeft={false}
+        name="Tara"
+        email="Tara@always-closed.com"
+        ig="@tara_Ink"
+      />
+      <ArtistItem
+        orientedLeft={true}
+        name="chutters"
+        email="Chutters@always-closed.com"
+        ig="@chutters_freestyle_ink"
+      />
+    </section>
   );
 }
