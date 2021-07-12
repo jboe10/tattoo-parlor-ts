6713ae799ee1968e92abb78b941b053356c67646
jboe10 <jboesurf@gmail.com>
2021-07-12 16:48:06 -0700

    responsive footer added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { About } from './components/about/About';
 import { Artists } from './components/artists/Artists';
 import { Gallery } from './components/gallery/Gallery';
 import { Home } from './components/home/Home';
+import { Footer } from './components/Footer';
 import './styles/main.sass';
 function App() {
 	return (
 		<div className="App">
 			<Home />
-			<Gallery />
+			{/* <Gallery /> */}
 			<Artists />
 			<About />
+			<Footer />
 		</div>
 	);
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-interface footerProps {
+interface footerProps {}
 
-}
-
-export const footer: React.FC<footerProps> = () => {
-  return (
-    <>
-    </>
-  );
-}
+export const Footer: React.FC<footerProps> = () => {
+	return (
+		<footer>
+			<h3>Always Closed</h3>
+			<nav className="nav-desktop">
+				<a href="/">Gallery</a>
+				<a href="/">Artists</a>
+				<a href="/">Our Shop</a>
+				<a href="/">Contact</a>
+			</nav>
+		</footer>
+	);
+};
